779d8ded3b554580211abf2ecde63fedc913b918
vatsaaa <ankur.vatsa@gmail.com>
2023-11-27 01:25:37 +0530

    Making intro and conclusion slightly better

--- utils/conclusion.py
+++ utils/conclusion.py
@@ -2,7 +2,7 @@ conclusion = {
     "Hindi": 'धन्यवाद मेरे प्यारे नन्हें श्रोताओं! आशा है यह कहानी आपको पसंद आई होगी, इसकी सीख अवश्य याद रखना!'
             'अपने पॉडकास्ट को subscribe अवश्य करें, और हमारी हर कहानी सुनें।'
             'फिर मिलेंगे, अगली कहानी में। तब तक अपना ध्यान रखो, अच्छे से पढ़ो, माँ-पापा का कहना मानो और सदा मुस्कराते रहो!',
-    "English": 'Thank you, my dear little audience! I hope you enjoyed this story; make sure you remember its moral always!'
-                'Be sure to subscribe to our podcast and listen to every story.'
-                'Until next time, take care, study well, obey your parents, and always keep smiling!'
+    "English": 'Thank you, my dear precious audience! I hope you enjoyed this story; make sure you remember its moral always!'
+                'Be sure to subscribe to our podcast and listen to a new story everyday.'
+                'Until next time! Take care, study well, obey your elders, and remember your smile makes the world better!'
 }
